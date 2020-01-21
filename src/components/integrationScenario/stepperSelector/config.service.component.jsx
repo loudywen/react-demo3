0c6cf6c481bb000e905bc99e0863fb7cc36a128f
diwenlao <loudywen@gmail.com>
2020-01-21 11:23:06 -0600

    added stepper for testing pt3

--- src/components/integrationScenario/stepperSelector/config.service.component.jsx
+++ src/components/integrationScenario/stepperSelector/config.service.component.jsx
@@ -5,7 +5,8 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
 
 const ConfigService = ({open, cancel, hop, configService, pos}) => {
 
@@ -13,40 +14,86 @@ const ConfigService = ({open, cancel, hop, configService, pos}) => {
     const handleCancel = () => {
         cancel()
     };
-    console.log(pos)
+    console.log(pos);
 
-    console.log(hop.serviceContracts[pos])
+    console.log(hop.serviceContracts[pos]);
+
+    const serviceSchema = hop.serviceContracts[pos].serviceSchema;
+    const properties = serviceSchema["properties"];
+    console.log("properties", properties);
+
+    const extract = properties => {
+        let kv = [];
+        let keys = Object.keys(properties);
+        let values = Object.values(properties);
+        for (let i = 0; i < keys.length; i++) {
+            kv.push({[keys[i]]: values[i]})
+        }
+        return kv;
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // call configService
+    };
+
+    const changeHandler = () => {
+
+    };
+
     return (
         <div>
             <Dialog
                 disableBackdropClick
                 open={open}
-                // onClose={handleClose}
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
                 maxWidth="lg"
-                // PaperProps={{
-                //     style: {
-                //         width: '750px',
-                //     }
-                // }}
             >
-                <DialogTitle id="alert-dialog-title">{"Select Hop"}</DialogTitle>
-                <form>
-                    <DialogContent>
-                        Config serivce
-                    </DialogContent>
+                <DialogTitle
+                    id="alert-dialog-title">Configure {hop.serviceContracts[pos].serviceMetaData.serviceName}</DialogTitle>
 
-                    <DialogActions>
-                        <Button onClick={handleCancel} color="primary" autoFocus>
-                            Cancel
-                        </Button>
-                    </DialogActions>
+                <DialogContent style={{
+                    display: "flex",
+                    justifyContent: "center"
+                }}>
+                    <form onSubmit={handleSubmit}>
+                        <FormControl>
+                            {extract(properties).map((obj, index) => {
+                                let keys = Object.keys(obj);
+                                let values = Object.values(obj);
 
-                </form>
+                                return (
+                                    <TextField
+                                        style={{
+                                            minWidth: "300px"
+                                        }}
+                                        key={index}
+                                        label={keys[0]}
+                                        type={values[0].type === "integer" ? "number" : values[0].type === "string" ? "text" : "boolean"}
+                                        name={keys[0]}
+                                        required={values[0].required}
+                                        onChange={changeHandler}/>
+                                )
+                            })}
+
+                        </FormControl>
+
+
+                        <DialogActions>
+                            <Button type="submit" color="primary">
+                                Submit
+                            </Button>
+                            <Button onClick={handleCancel} color="primary" autoFocus>
+                                Cancel
+                            </Button>
+                        </DialogActions>
+                    </form>
+                </DialogContent>
+
             </Dialog>
         </div>
     )
-}
+};
 
 export default ConfigService
